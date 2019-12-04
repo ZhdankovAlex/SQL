6a463c019bd821a72023013ae61ddb24988830f5
Alexey <Alexey@ALEX-HP>
2019-12-04 20:40:54 +0300

    Вторая лабораторная работа по SQL

---
+++ Laba2/Laba2/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Laba2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 40);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(196, 238);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(210, 40);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(196, 238);
+            this.listBox2.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(407, 40);
+            this.listBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(228, 238);
+            this.listBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(640, 40);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "запрос 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(639, 90);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(72, 35);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "запрос 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(639, 140);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(72, 35);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "запрос 3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(639, 188);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(72, 35);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "запрос 4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(639, 240);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(72, 35);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "запрос 5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(89, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "CAR";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(281, 20);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "INFO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(465, 20);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "RESULT";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(714, 51);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(227, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Вывод автомобилей с кузовом типа \"купе\"";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(714, 101);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(347, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Вывод автомобилей с мощностью больше 100, но меньше 200 л.с.";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(714, 151);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(211, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Вывод авто, сгруппированных по марке";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(714, 199);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(326, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Вывод марки и модели авто, объём двигателя которых = 1.6 л";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(714, 251);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(268, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Вывод внедорожников с мощностью выше 120 л.с.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1063, 296);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Laba2/Laba2/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Laba2
+{
+    public partial class Form1 : Form
+    {
+        class Car
+        {
+            public string code { get; set; }
+            public string brand { get; set; }
+            public string model { get; set; }
+            public Car(string c, string b, string m)
+            {
+                code = c;
+                brand = b;
+                model = m;
+            }
+            public override string ToString()
+            {
+                return code + ": " + brand + " " + model;
+            }
+        }
+
+        class Info
+        {
+            public string code { get; set; }
+            public string bodywork { get; set; }
+            public string engine_volume { get; set; }
+            public string power { get; set; }
+            public Info(string c, string b, string e, string p)
+            {
+                code = c;
+                bodywork = b;
+                engine_volume = e;
+                power = p;
+            }
+            public override string ToString()
+            {
+                return code + ": " + bodywork + " " + engine_volume + " " + power;
+            }
+        }
+
+        List<Car> lc;
+        List<Info> li;
+
+        public void ReadCar()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader("brandAndModel.txt"))
+                {
+                    string line;
+                    string[] fields;
+                    line = sr.ReadLine();
+                    while (line != null)
+                    {
+                        fields = line.Split(';');
+                        lc.Add(new Car(fields[0], fields[1], fields[2]));
+                        listBox1.Items.Add(line);
+                        line = sr.ReadLine();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add(ex.Message);
+            }
+        }
+
+        public void ReadInfo()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader("characteristic.txt"))
+                {
+                    string line;
+                    string[] fields;
+                    line = sr.ReadLine();
+                    while (line != null)
+                    {
+                        fields = line.Split(';');
+                        li.Add(new Info(fields[0], fields[1], fields[2], fields[3]));
+                        listBox2.Items.Add(line);
+                        line = sr.ReadLine();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                listBox2.Items.Clear();
+                listBox2.Items.Add(ex.Message);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            lc = new List<Car>();
+            ReadCar();
+            li = new List<Info>();
+            ReadInfo();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //Вывод автомобилей с кузовом типа "купе"
+            listBox3.Items.Clear();
+            var result = from car in lc
+                         from info in li
+                         where info.bodywork == " coupe" && info.code == car.code
+                         select car;
+            foreach (var item in result)
+            {
+                listBox3.Items.Add(item.ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Вывод автомобилей с мощностью больше 100, но меньше 200 л.с.
+            listBox3.Items.Clear();
+            var result = from car in lc
+                         from info in li
+                         where int.Parse(info.power.Substring(info.power.IndexOf(" ") + 1, info.power.Length - info.power.IndexOf(" ") - 5)) > 100 &&
+                               int.Parse(info.power.Substring(info.power.IndexOf(" ") + 1, info.power.Length - info.power.IndexOf(" ") - 5)) < 200 &&
+                               info.code == car.code
+                         select car;
+            foreach (var item in result)
+            {
+                listBox3.Items.Add(item.ToString());
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Вывод авто, сгруппированных по марке
+            listBox3.Items.Clear();
+            var result = from car in lc
+                         group car by car.brand;
+            foreach (IGrouping<string, Car> car in result)
+            {
+                listBox3.Items.Add(" " + car.Key);
+                foreach (var item in car)
+                {
+                    listBox3.Items.Add(item.ToString());
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Вывод марки и модели авто, объём двигателя которых = 1.6 л
+            listBox3.Items.Clear();
+            var result = from car in lc
+                         from info in li
+                         where info.engine_volume == " 1.6 l" && car.code == info.code
+                         select car;
+            foreach (var item in result)
+            {
+                listBox3.Items.Add(item.brand + " " + item.model);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //Вывод внедорожников с мощностью выше 120 л.с.
+            listBox3.Items.Clear();
+            var result = from car in lc
+                         from info in li
+                         where int.Parse(info.power.Substring(info.power.IndexOf(" ") + 1, info.power.Length - info.power.IndexOf(" ") - 5)) > 120 &&
+                               info.bodywork == " suv" &&
+                               info.code == car.code 
+                         select car;
+            foreach (var item in result)
+            {
+                listBox3.Items.Add(item.ToString());
+            }
+        }
+    }
+}
